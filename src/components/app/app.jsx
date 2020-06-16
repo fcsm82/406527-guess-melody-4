a8deea7d32d651a2fb5912a3b132ade37a5242f1
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-06-16 12:11:20 -0400

    Implement routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,9 @@
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import React from "react";
 import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import QuestionArtistScreen from "../question-artist-screen/question-artist-screen.jsx";
+import QuestionGenreScreen from "../question-genre-screen/question-genre-screen.jsx";
 
 const welcomeButtonHandler = () => {};
 
@@ -8,12 +11,25 @@ const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={welcomeButtonHandler}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorsCount={errorsCount}
+            onWelcomeButtonClick={welcomeButtonHandler}
+          />
+        </Route>
+        <Route exact path="/dev-artist">
+          <QuestionArtistScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <QuestionGenreScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
+
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
