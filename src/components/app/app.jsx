2bbf6aae5ce12c4219a0e663e21537ec246b7962
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-06-17 13:19:40 -0400

    Mocks, updated tests, switching screens

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,38 +1,94 @@
+import React, {PureComponent} from "react";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import React from "react";
 import PropTypes from "prop-types";
+import {GameType} from "../../const.js";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import QuestionArtistScreen from "../question-artist-screen/question-artist-screen.jsx";
 import QuestionGenreScreen from "../question-genre-screen/question-genre-screen.jsx";
 
-const welcomeButtonHandler = () => {};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
 
-const App = (props) => {
-  const {errorsCount} = props;
+    this.state = {
+      step: -1,
+    };
+  }
 
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <WelcomeScreen
-            errorsCount={errorsCount}
-            onWelcomeButtonClick={welcomeButtonHandler}
-          />
-        </Route>
-        <Route exact path="/dev-artist">
-          <QuestionArtistScreen />
-        </Route>
-        <Route exact path="/dev-genre">
-          <QuestionGenreScreen />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
-};
+  _renderGameScreen() {
+    const {errorsCount, questions} = this.props;
+    const {step} = this.state;
+    const question = questions[step];
+    if (step === -1 || step >= questions.length) {
+      return (
+        <WelcomeScreen
+          errorsCount={errorsCount}
+          onWelcomeButtonClick={() => {
+            this.setState({
+              step: 0,
+            });
+          }}
+        />
+      );
+    }
 
+    if (question) {
+      switch (question.type) {
+        case GameType.ARTIST:
+          return (
+            <QuestionArtistScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+        case GameType.GENRE:
+          return (
+            <QuestionGenreScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+      }
+    }
 
+    return null;
+  }
+
+  render() {
+    const {questions} = this.props;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderGameScreen()}
+          </Route>
+          <Route exact path="/artist">
+            <QuestionArtistScreen
+              question={questions[1]}
+              onAnswer={() => {}}
+            />
+          </Route>
+          <Route exact path="/genre">
+            <QuestionGenreScreen
+              question={questions[0]}
+              onAnswer={() => {}}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  questions: PropTypes.array.isRequired,
 };
-
 export default App;
--- src/components/question-genre-screen/question-genre-screen.test.js
+++ src/components/question-genre-screen/question-genre-screen.test.js
@@ -1,13 +1,36 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import QuestionGenreScreen from "./question-genre-screen.jsx";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import QuestionGenreScreen from './question-genre-screen.jsx';
 
-it(`Should QuestionGenreScreen render correctly`, () => {
-  const tree = renderer
-    .create(<QuestionGenreScreen
+const question = {
+  type: `genre`,
+  genre: `rock`,
+  answers: [{
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `rock`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `blues`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `jazz`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `rock`,
+  }],
+};
 
-    />)
-    .toJSON();
+it(`QuestionGenreScreen is rendered correctly`, () => {
+  const tree = renderer.create((
+    <QuestionGenreScreen
+      question={question}
+      onAnswer={() => {}}
+    />
+  ), {
+    createNodeMock: () => {
+      return {};
+    }
+  }).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
